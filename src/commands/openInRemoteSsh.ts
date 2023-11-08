216745407348c15fbd651066b66f1c037dbbbd70
Andy Jordan <2226434+andyleejordan@users.noreply.github.com>
2023-11-08 10:56:43 -0800

    Clean up `pickArcEnabledServer.ts`

--- src/commands/openInRemoteSsh.ts
+++ src/commands/openInRemoteSsh.ts
@@ -5,7 +5,7 @@ import { readFile, writeFile } from "fs/promises";
 import { IActionContext } from "@microsoft/vscode-azext-utils";
 import { parse } from "ssh-config";
 import { commands } from "vscode";
-import { pickArcEnabledServer as pickArcEnabledServersResource } from "../utils/pickArcEnabledServer";
+import { pickArcEnabledServer } from "../utils/pickArcEnabledServer";
 import { verifyRemoteSshExtension } from "../utils/verifyRemoteSshExtension";
 import { runAzSshConfigCommand } from "../utils/runAzSshConfigCommand";
 import { sshConfigPath } from "../constants";
@@ -16,7 +16,7 @@ export async function openInRemoteSsh(
     node?: ArcEnabledServerItem): Promise<void> {
     // This was either called on a resource or from the command palette, in
     // which case we need to pick one.
-    node ??= await pickArcEnabledServersResource(context);
+    node ??= await pickArcEnabledServer(context);
 
     // Verify that the Remote - SSH extension is installed and activated.
     await verifyRemoteSshExtension(context);
--- src/utils/pickArcEnabledServer.ts
+++ src/utils/pickArcEnabledServer.ts
@@ -1,31 +1,21 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { AzureResourceQuickPickWizardContext, AzureWizardPromptStep, IActionContext, QuickPickAzureResourceStep, QuickPickAzureSubscriptionStep, QuickPickGroupStep, QuickPickWizardContext, runQuickPickWizard } from "@microsoft/vscode-azext-utils";
+import { IActionContext, QuickPickAzureResourceStep, QuickPickAzureSubscriptionStep, QuickPickGroupStep, runQuickPickWizard } from "@microsoft/vscode-azext-utils";
 import { AzExtResourceType } from "@microsoft/vscode-azureresources-api";
 import { ext } from "../extensionVariables";
 import { ArcEnabledServerItem } from "../ArcEnabledServerItem";
 
-function getPickArcEnabledServerPromptSteps(): AzureWizardPromptStep<AzureResourceQuickPickWizardContext>[] {
+export async function pickArcEnabledServer(context: IActionContext): Promise<ArcEnabledServerItem> {
     const treeDataProvider = ext.rgApiV2.resources.azureResourceTreeDataProvider;
     const types = [AzExtResourceType.ArcEnabledServers];
-    return [
+    const promptSteps = [
         new QuickPickAzureSubscriptionStep(treeDataProvider),
         new QuickPickGroupStep(treeDataProvider, { groupType: types }),
         new QuickPickAzureResourceStep(
             treeDataProvider,
-            {
-                // TODO: Somehow filter to only running servers?
-                resourceTypes: types,
-                skipIfOne: true,
-            },
+            { resourceTypes: types, skipIfOne: false },
             { placeHolder: "Pick an Arc-enabled Server" }),
-    ];
-}
-
-export async function pickArcEnabledServer(context: IActionContext): Promise<ArcEnabledServerItem> {
-    const promptSteps: AzureWizardPromptStep<QuickPickWizardContext>[] = [
-        ...getPickArcEnabledServerPromptSteps()
     ];
 
     return await runQuickPickWizard(context, { promptSteps, title: "Pick an Arc-enabled Server" });
