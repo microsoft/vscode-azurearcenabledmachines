a512fe3ba2d9a376d6e161c6ecaf6403f5c2d213
Andy Jordan <2226434+andyleejordan@users.noreply.github.com>
2024-01-30 17:53:27 -0800

    Setup CI with unit testing framework

---
+++ .vscode-test.mjs
@@ -0,0 +1,13 @@
+import os from "os";
+import path	from "path";
+import { defineConfig } from "@vscode/test-cli";
+
+export default defineConfig({
+	files: "out/test/**/*.test.js",
+	// The default user data directory had too many characters for the IPC connection on macOS in CI.
+	launchArgs: [ "--profile-temp", "--user-data-dir", path.join(os.tmpdir(), "vscode-user-data") ],
+	mocha: {
+		ui: "bdd", // describe, it, etc.
+		timeout: 60 * 1000 // 10 minutes to allow for debugging
+	},
+});
