c21736b921f5dce219038fca9ac5c978c8ce84f4
Andy Jordan <2226434+andyleejordan@users.noreply.github.com>
2024-02-06 20:02:07 -0800

    Add error handling around Azure CLI

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,8 @@ import { homedir } from "os";
 import { promisify } from "util";
 import { exec } from "child_process";
 
+export const getAzureCLI = "https://aka.ms/GetTheAzureCLI";
+export const loginToAzureCLI = "https://learn.microsoft.com/en-us/cli/azure/authenticate-azure-cli-interactively";
 export const remoteSshExtensionId = "ms-vscode-remote.remote-ssh";
 export const sshConfigPath: string = join(homedir(), ".ssh", "config");
 export const execAsync = promisify(exec);
---
+++ src/utils/verifyAzureCLI.ts
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { IActionContext } from "@microsoft/vscode-azext-utils";
+import { ext } from "../extensionVariables";
+import { getAzureCLI, loginToAzureCLI } from "../constants";
+import { execAsync } from "../constants";
+
+export async function verifyAzureCLI(context: IActionContext): Promise<void> {
+    try {
+        const { stdout, stderr } = await execAsync("az --version");
+        ext.outputChannel.appendLog(stdout, { resourceName: "az --version stdout" });
+        ext.outputChannel.appendLog(stderr, { resourceName: "az --version stderr" });
+    } catch (error) {
+        void context.ui.showWarningMessage(
+            `You must have the [Azure CLI](${getAzureCLI}) installed to perform this operation.`,
+            { learnMoreLink: getAzureCLI});
+        context.errorHandling.suppressDisplay = true;
+        throw new Error("Azure CLI is not installed.");
+    }
+
+    try {
+        const { stdout, stderr } = await execAsync("az account show");
+        ext.outputChannel.appendLog(stdout, { resourceName: "az account show" });
+        ext.outputChannel.appendLog(stderr, { resourceName: "az account show" });
+    } catch (error) {
+        void context.ui.showWarningMessage(
+            "You must log in to the Azure CLI with `az login` in the [Terminal](command:workbench.action.terminal.new) to perform this operation.",
+            { learnMoreLink: loginToAzureCLI} );
+        context.errorHandling.suppressDisplay = true;
+        throw new Error("Azure CLI is not logged in.");
+    }
+}
