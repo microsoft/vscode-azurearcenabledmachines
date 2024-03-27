b311a009daa5dcbeac25d9e796ca31523df0e1b0
Andy Jordan <2226434+andyleejordan@users.noreply.github.com>
2024-03-27 15:47:27 -0700

    Fix output included in packaged extension
    Had to manually unzip and inspect it...found we were bundling unnecessary files.
    TSC output is now left to the default location instead of `out`.

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -3,7 +3,7 @@ import path	from "path";
 import { defineConfig } from "@vscode/test-cli";
 
 export default defineConfig({
-	files: "out/test/**/*.test.js",
+	files: "src/test/**/*.test.js",
 	// The default user data directory had too many characters for the IPC connection on macOS in CI.
 	launchArgs: [ "--profile-temp", "--user-data-dir", path.join(os.tmpdir(), "vscode-user-data") ],
 	mocha: {
