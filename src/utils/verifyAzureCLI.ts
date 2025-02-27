ac2a96429b74a3dffe2799a5fd7a8598734b8b73
Andy Jordan <2226434+andyleejordan@users.noreply.github.com>
2025-02-27 10:50:15 -0800

    Fix unused error from catch

--- src/utils/verifyAzureCLI.ts
+++ src/utils/verifyAzureCLI.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import { ExecException } from "child_process";
 import { IActionContext } from "@microsoft/vscode-azext-utils";
 import { ext } from "../extensionVariables";
 import { getAzureCLI, loginToAzureCLI } from "../constants";
@@ -12,9 +13,10 @@ export async function verifyAzureCLI(context: IActionContext): Promise<void> {
         ext.outputChannel.appendLog(stdout, { resourceName: "az --version stdout" });
         ext.outputChannel.appendLog(stderr, { resourceName: "az --version stderr" });
     } catch (error) {
+        ext.outputChannel.appendLog((error as ExecException).message, { resourceName: "az --version error" });
         void context.ui.showWarningMessage(
             `You must have the [Azure CLI](${getAzureCLI}) installed to perform this operation.`,
-            { learnMoreLink: getAzureCLI});
+            { learnMoreLink: getAzureCLI });
         context.errorHandling.suppressDisplay = true;
         throw new Error("Azure CLI is not installed.");
     }
@@ -24,9 +26,10 @@ export async function verifyAzureCLI(context: IActionContext): Promise<void> {
         ext.outputChannel.appendLog(stdout, { resourceName: "az account show" });
         ext.outputChannel.appendLog(stderr, { resourceName: "az account show" });
     } catch (error) {
+        ext.outputChannel.appendLog((error as ExecException).message, { resourceName: "az account show error" });
         void context.ui.showWarningMessage(
             "You must log in to the Azure CLI with `az login` in the [Terminal](command:workbench.action.terminal.new) to perform this operation.",
-            { learnMoreLink: loginToAzureCLI} );
+            { learnMoreLink: loginToAzureCLI });
         context.errorHandling.suppressDisplay = true;
         throw new Error("Azure CLI is not logged in.");
     }
